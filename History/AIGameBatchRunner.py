2d08fae156883e76c73eaa98b60316878cb464d8
Daniel Brey <danpbrey@gmail.com>
2025-03-30 14:08:32 -0400

    Deleted History Directory from git

--- History/AIGameBatchRunner.py
+++
@@ -1,6 +0,0 @@
-from AIGame import AIGame
-
-with open("AIGameBatchRunner.py") as my_file:
-    for i in range(1000):
-        game = AIGame()
-        game.PlayGame()
--- History/Services/ActionLogger.py
+++
@@ -1,5 +0,0 @@
-from Objects.Action import Action
-
-class ActionLogger:
-    def logAction(action: Action, actionLog: list[Action]) -> None:
-        actionLog.append(action)
